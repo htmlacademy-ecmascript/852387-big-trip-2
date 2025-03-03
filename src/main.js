3d99c609fcd5a8facd673f00a73ea3d673f03797
2tankov2 <domino-82@mail.ru>
2025-03-03 17:13:49 +0300

    разбивает код на модули и добавляет презентер точек маршрута

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import SortView from './view/sort-view.js';
+import FilterView from './view/filter-view.js';
+import {render} from './render.js';
+import BoardPointsPresenter from './presenter/board-points-presenter.js';
+
+const body = document.body;
+
+const siteHeaderElement = body.querySelector('.page-header');
+const tripControlFilter = siteHeaderElement.querySelector('.trip-controls__filters');
+
+render(new FilterView(), tripControlFilter);
+
+const siteMainElement = document.querySelector('.page-main');
+const containerTripEvents = siteMainElement.querySelector('.trip-events');
+
+render(new SortView(), containerTripEvents);
+
+const boardPointsPresenter = new BoardPointsPresenter({boardPointsContainer: containerTripEvents});
+
+boardPointsPresenter.init();
+
+
+// render(new PointListView(), containerTripEvents);
+
+// const listTripEvents = siteMainElement.querySelector('.trip-events__list');
+
+// render(new PointEditView(), listTripEvents);
+
+// render(new PointAddView(), listTripEvents);
+
+
