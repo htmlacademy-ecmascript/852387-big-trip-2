aed20d8a3ed01c22c1732703549ee6bceb22b10d
2tankov2 <domino-82@mail.ru>
2025-04-09 07:09:54 +0300

    добавит презентер фильтров

--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
 //import SortView from './view/sort-view.js';
-import FilterView from './view/filter-view.js';
-import NewPointButtonView from './view/new-point-button-view.js';
-import { render } from './framework/render.js';
+//import FilterView from './view/filter-view.js';
+//import NewPointButtonView from './view/new-point-button-view.js';
+//import { render } from './framework/render.js';
 import BoardPointsPresenter from './presenter/board-points-presenter.js';
+import FilterPresenter from './presenter/filter-presenter.js';
 import PointsModel from './model/point-model.js';
 import FilterModel from './model/filter-model.js';
 //import { generateFilter } from './mock/filter.js';
 
-const filters = [
-  {
-    type: 'all',
-    count: 0,
-  },
-];
+// const filters = [
+// {
+// type: 'all',
+// count: 0,
+// },
+// ];
 
 const body = document.body;
 const siteHeaderElement = body.querySelector('.page-header');
@@ -27,11 +28,11 @@ pointsModel.init();
 //const filters = generateFilter(pointsModel.points);
 
 //render(new FilterView({ filters }), tripControlFilter);
-render(new FilterView({
-  filters,
-  currentFilterType: 'all',
-  onFilterTypeChange: () => {}
-}), tripMain);
+// render(new FilterView({
+// filters,
+// currentFilterType: 'all',
+// onFilterTypeChange: () => {}
+// }), tripMain);
 
 //render(new NewPointButtonView(), tripMain);
 
@@ -40,4 +41,11 @@ const boardPointsPresenter = new BoardPointsPresenter({
   pointsModel,
 });
 
+const filterPresenter = new FilterPresenter({
+  filterContainer: tripMain,
+  filterModel,
+  pointsModel
+});
+
+filterPresenter.init();
 boardPointsPresenter.init();
