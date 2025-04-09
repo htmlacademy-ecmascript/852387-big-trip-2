6f9f62a4c45ff25535d9e6fbec2e2cf86e0a22ab
2tankov2 <domino-82@mail.ru>
2025-04-09 06:25:11 +0300

    удаляет моки для фильтров, шаблон фильтров учитывает выбранный фильтр, реализует механизм подписки на выбор нового фильтра

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 //import SortView from './view/sort-view.js';
 import FilterView from './view/filter-view.js';
+import NewPointButtonView from './view/new-point-button-view.js';
 import { render } from './framework/render.js';
 import BoardPointsPresenter from './presenter/board-points-presenter.js';
 import PointsModel from './model/point-model.js';
 import FilterModel from './model/filter-model.js';
-import { generateFilter } from './mock/filter.js';
+//import { generateFilter } from './mock/filter.js';
+
+const filters = [
+  {
+    type: 'all',
+    count: 0,
+  },
+];
 
 const body = document.body;
 const siteHeaderElement = body.querySelector('.page-header');
-const tripControlFilter = siteHeaderElement.querySelector('.trip-controls__filters');
+const tripMain = siteHeaderElement.querySelector('.trip-main');
 const siteMainElement = document.querySelector('.page-main');
 const containerTripEvents = siteMainElement.querySelector('.trip-events');
 
@@ -16,9 +24,16 @@ const pointsModel = new PointsModel();
 const filterModel = new FilterModel();
 pointsModel.init();
 
-const filters = generateFilter(pointsModel.points);
+//const filters = generateFilter(pointsModel.points);
 
-render(new FilterView({ filters }), tripControlFilter);
+//render(new FilterView({ filters }), tripControlFilter);
+render(new FilterView({
+  filters,
+  currentFilterType: 'all',
+  onFilterTypeChange: () => {}
+}), tripMain);
+
+//render(new NewPointButtonView(), tripMain);
 
 const boardPointsPresenter = new BoardPointsPresenter({
   container: containerTripEvents,
